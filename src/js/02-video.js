6a73164e9ead04a7ec4d1027df50686852b9744b
Alexandr Hrynenko <layt.lat@ukr.net>
2023-03-06 11:06:11 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoPlayer = document.querySelector('#vimeo-player');
+
+const player = new Player(videoPlayer);
+
+player.on('timeupdate', throttle(onPlayClick, 1000));
+
+function onPlayClick(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  );
+}
